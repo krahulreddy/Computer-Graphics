f6ff1de3b647a1a9191236548c71521472644b42
K Rahul Reddy <k_rahul_reddy@outlook.com>
2020-01-30 15:34:08 +0530

    Updated cybertruck model

--- 3001/circle.c
+++ 3001/circle.c
@@ -4,6 +4,8 @@
 #define H 720
 #define W 720
 
+int X = 0, rA = 0;
+
 void acute_bresenham(int x1, int y1, int x2, int y2)
 {
 	int dx = x2 - x1, dy = y2 - y1;
@@ -71,26 +73,29 @@ void bresenham(int x1, int y1, int x2, int y2)
 	}
 }
 
-void draw(cx, cy, x, y)
+void draw(cx, cy, x, y, semi)
 {
 	glBegin(GL_POINTS);
-	glVertex2i(cx + x, cx + y);
-	glVertex2i(cx + x, cx - y);
-	glVertex2i(cx - x, cx + y);
-	glVertex2i(cx - x, cx - y);
-	glVertex2i(cx + y, cx + x);
-	glVertex2i(cx + y, cx - x);
-	glVertex2i(cx - y, cx + x);
-	glVertex2i(cx - y, cx - x);
+	glVertex2i(cx + x, cy + y);
+	glVertex2i(cx - x, cy + y);
+	glVertex2i(cx + y, cy + x);
+	glVertex2i(cx - y, cy + x);
+	if(!semi)
+	{
+		glVertex2i(cx - y, cy - x);
+		glVertex2i(cx + y, cy - x);
+		glVertex2i(cx - x, cy - y);
+		glVertex2i(cx + x, cy - y);
+	}
 	glEnd();
 }
 
-void circle(cx, cy, r)
+void circle(cx, cy, r, semi)
 {
 	int x = 0, y = r, d = 3 - 2 * r;
 	while(y >= x)
 	{
-		printf("%d %d\n", x, y);
+//		printf("%d %d\n", x, y);
 		x++;
 		if(d <= 0)
 		{
@@ -101,39 +106,83 @@ void circle(cx, cy, r)
 			y--;
 			d = d + 4 * (x - y) + 10;
 		}
-		draw(cx, cy, x, y);
+		draw(cx, cy, x, y, semi);
 	}
+}
+
+void wheel(cx, cy, r)
+{
+	int i;
+	// Outer Circle
+	glColor3f(0.0, 1.0, 0.4);
+	circle(cx, cy, r, 0);
+
+
+
+	// Inner circle
+	glColor3f(1.0, 0.0, 0.0);
+	circle(cx, cy, r / 3, 0);
+
+	// Lines		
+	bresenham(cx, cy + r / 3, cx, cy + r);
+	bresenham(cx, cy - r / 3, cx, cy - r);
+	bresenham(cx + r / 3, cy, cx + r, cy);
+	bresenham(cx - r / 3, cy, cx - r, cy);
+
+	bresenham(cx + r / 3 / 1.4, cy + r / 3 / 1.4, cx + r / 1.4, cy + r / 1.4);
+	bresenham(cx + r / 3 / 1.4, cy - r / 3 / 1.4, cx + r / 1.4, cy - r / 1.4);
+	bresenham(cx - r / 3 / 1.4, cy + r / 3 / 1.4, cx - r / 1.4, cy + r / 1.4);
+	bresenham(cx - r / 3 / 1.4, cy - r / 3 / 1.4, cx - r / 1.4, cy - r / 1.4);
 }
 void display()
 {
 	glPointSize(1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	int i, r = 200;
-
-	// Outer Circle
-	glColor3f(0.0, 1.0, 0.4);
-	circle(0, 0, r);
+	
+	int wheelSize = 200, wheelDistance = 300;
 
 	// Ground
+	int i = 0;
 	glBegin(GL_POINTS);
-	for(i = -2 * r; i < 2 * r; i++)
-		glVertex2i(i, -r);
+	for(i = -2000; i < 2000; i++)
+		glVertex2i(i, -wheelSize);
+
 	glEnd();
+	glPushMatrix();
+		glTranslatef(-wheelDistance + X, 0, 0);
+		glRotatef(-rA, 0, 0, 1);
+		glTranslatef(wheelDistance - X, 0, 0);
+		wheel(-wheelDistance + X, 0, wheelSize);
+	glPopMatrix();
+	glTranslatef(0, 0, 0);
+	glPushMatrix();
+		glTranslatef(wheelDistance + X, 0, 0);
+		glRotatef(-rA, 0, 0, 1);
+		glTranslatef(-wheelDistance - X, 0, 0);
+		wheel(wheelDistance + X, 0, wheelSize);
+	glPopMatrix();
 
-	// Inner circle
-	glColor3f(1.0, 0.0, 0.0);
-	circle(0, 0, r / 3);
-
-	// Lines
-	bresenham(0, r / 3, 0, r);
-	bresenham(0, -r / 3, 0, -r);
-	bresenham(r / 3, 0, r, 0);
-	bresenham(-r / 3, 0, -r, 0);
-	//bresenham(, r / 6, r, r);
-	
+	int edge = wheelDistance + wheelSize;
+	// Car Body
+	bresenham(X, edge, -750 + X, wheelDistance);
+	bresenham(-750 + X, wheelDistance, -700 + X, 0);
+	bresenham(-520 + X, 0, -700 + X, 0);
+	circle(-wheelDistance + X, 0, 220, 1);
+	bresenham(-80 + X, 0, 80 + X, 0);
+	circle(wheelDistance + X, 0, 220, 1);
+	bresenham(520 + X, 0, 700 + X, 0);
+	bresenham(700 + X, 0, 700 + X, wheelSize);
+	bresenham(700 + X, wheelSize, X, 500);
 
 	glFlush();
+}
+
+void update( int n) {
+	X += 2;
+	rA ++;
+//	glRotatef(10, 0, 0, 1);
+	glutPostRedisplay();
+	glutTimerFunc(50, update, 0);
 }
 
 int main(int argc, char** argv)
@@ -145,5 +194,6 @@ int main(int argc, char** argv)
 	gluOrtho2D(-1000, 1000, -800, 800);
 	glClearColor(0,0,0,1);
 	glutDisplayFunc(display);
+	glutTimerFunc(50, update, 0);
 	glutMainLoop();
 }
